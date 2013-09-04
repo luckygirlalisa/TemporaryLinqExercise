54e5212d868a42a9d77620fdfa56a6d38798c031
unknown <zfxiao@zfxiao.corporate.thoughtworks.com>
2013-09-04 09:05:12 +0800

    select person from people with Find, Count, FirstOrDefault, Where

---
+++ LinqExercise/ManagePeople.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercise
+{
+    public class ManagePeople
+    {
+        public Person FindPeopleWithNameLengthLargerThanFive(List<Person> people)
+        {
+            return people.Find(p => p.Name.Length > 5);
+        }
+
+        public int FindCountApplyRules(List<Person> people)
+        {
+            return people.Count(p => p.Age > 25);
+        }
+
+        public Person FindFirstPeopleOlderThan25(List<Person> people)
+        {
+            return people.FirstOrDefault(p => p.Age > 25);
+        }
+
+        public List<Person> FindPersonListOlderThan25(List<Person> people)
+        {
+            return people.Where(p => p.Age > 25).ToList();
+        }
+//
+//        public List<Person> FindPersonListOlderThan25WithSelect(List<Person> people)
+//        {
+//            return people.Select(p => p.Age > 25).ToList();
+//        }
+    }
+}
---
+++ LinqExercise/ManagePeopleTest.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LinqExercise
+{
+    class ManagePeopleTest
+    {
+        private List<Person> people;
+        private ManagePeople managePeople;
+        private Person person1;
+        private Person person2;
+
+        [SetUp]
+        public void Init()
+        {
+            person1 = new Person { Name = "Xiaohong", Age = 26 };
+            person2 = new Person {Name = "Xiaohong", Age = 28};
+
+            people = new List<Person>
+                {
+                    new Person {Name = "Xiao", Age = 20 },
+                    person1,
+                    new Person {Name = "Lily", Age = 23 },
+                    person2
+                };
+            managePeople = new ManagePeople();
+        }
+
+        [Test]
+        public void Should_get_first_person_with_name_length_larger_than_5_with_Find()
+        {
+            var findedPeople = managePeople.FindPeopleWithNameLengthLargerThanFive(people);
+
+            Assert.NotNull(findedPeople);
+            Assert.That(findedPeople, Is.EqualTo(person1));
+        }
+
+        [Test]
+        public void Should_get_first_person_with_age_larger_than_25_with_FirstOrDefault()
+        {
+            var firstPersonOlderThan25 = managePeople.FindFirstPeopleOlderThan25(people);
+            Assert.That(firstPersonOlderThan25, Is.EqualTo(person1));
+        }
+
+        [Test]
+        public void Should_get_people_list_with_age_larger_than_25_with_Count()
+        {
+            var findedPersonNum = managePeople.FindCountApplyRules(people);
+            Assert.That(findedPersonNum, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Should_find_person_list_older_than_25_with_Where()
+        {
+            var findedPeople = managePeople.FindPersonListOlderThan25(people);
+            var expectedPeople = new List<Person> { person1, person2 };
+            Assert.That(findedPeople, Is.EqualTo(expectedPeople));
+        }
+
+//        [Test, Ignore] //(open after implement)
+//        public void Should_find_person_list_older_than_25_with_select()
+//        {
+//            var findedPeople = managePeople.FindPersonListOlderThan25WithSelect(people);
+//            var expectedPeople = new List<Person> {person1, person2};
+//            Assert.That(findedPeople, Is.EqualTo(expectedPeople));
+//        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
